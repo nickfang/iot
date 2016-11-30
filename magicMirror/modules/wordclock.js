31a56756ea42f7678368336e75e17fc58d8dfcb4
nfang <fang.nicholas@gmail.com>
2016-11-30 12:43:11 -0600

    I have the word clock at a working point.  I also added a button that will show all the letters inbetween the time words.

--- magicMirror/modules/wordclock.js
+++ magicMirror/modules/wordclock.js
@@ -1,4 +1,5 @@
 const TIME_COLOR = "#000000";
+const IAN_COLOR = "#FF0000"
 
 function getCurrentTime(){
 	var d = new Date();
@@ -27,123 +28,73 @@ function getMinute(minute) {
 	// special case since the equation used in the switch case will put 0 in the five past case.
 	var ids = [];
 	var i;
+
+	// special case so that if minutes are 0, it won't fall into case 0.
 	if (minute == 0) {
-		console.log(minute + ": o'clock");
-		return;
-	}
-	switch(((minute-1)/5)|0) {
-		case 0:
-			console.log(minute + ": five past ");
-			ids = ["min5","past"];
-			break;
-		case 1:
-			console.log(minute + ": ten past ");
-			ids = ["min10", "past"];
-			break;
-		case 2:
-			console.log(minute + ": quarter past ");
-			ids = ["min15", "ian_a", "past"];
-			break;
-		case 3:
-			console.log(minute + ": twenty past ");
-			ids = ["min20", "past"];
-			break;
-		case 4:
-			console.log(minute + ": twenty-five past ");
-			ids = ["min20", "min5", "past"];
-			break;
-		case 5:
-			console.log(minute + ": half past ");
-			ids = ["min30", "past"];
-			break;
-		case 6:
-			console.log(minute + ": twenty-five to ");
-			ids = ["min20", "min5", "to"];
-			break;
-		case 7:
-			console.log(minute + ": twenty to ");
-			ids = ["min20", "to"];
-			break;
-		case 8:
-			console.log(minute + ": quarter to ");
-			ids = ["min15", "to"];
-			break;
-		case 9:
-			console.log(minute + ": ten to ");
-			ids = ["min10","to"];
-			break;
-		case 10:
-			console.log(minute + ": five to ");
-			ids = ["min5","to"];
-			break;
-		case 11:
-			console.log(minute + ": o'clock");
-			ids = ["oclock"];
-			break;
-		default:
-			console.log(minute + ": default - THIS IS WRONG!!!");
+		ids = ["o'clock"];
+	} else {
+		switch(((minute-1)/5)|0) {
+			case 0:
+				ids = ["min5","past"];
+				break;
+			case 1:
+				ids = ["min10", "past"];
+				break;
+			case 2:
+				ids = ["min15", "a", "past"];
+				break;
+			case 3:
+				ids = ["min20", "past"];
+				break;
+			case 4:
+				ids = ["min20", "min5", "past"];
+				break;
+			case 5:
+				ids = ["min30", "past"];
+				break;
+			case 6:
+				ids = ["min20", "min5", "to"];
+				break;
+			case 7:
+				ids = ["min20", "to"];
+				break;
+			case 8:
+				ids = ["min15", "a", "to"];
+				break;
+			case 9:
+				ids = ["min10","to"];
+				break;
+			case 10:
+				ids = ["min5","to"];
+				break;
+			case 11:
+				ids = ["oclock"];
+				break;
+			default:
+				console.log(minute + ": default - THIS IS WRONG!!!");
+		}
 	}
 	for (var i=0; i < ids.length; i++) {
-		console.log(ids);
 		document.getElementById(ids[i]).style.color=TIME_COLOR;
 	}
 }
 
 function getHour(hour) {
 		var id = "hour" + String(hour%12);
-		console.log(id);
 		document.getElementById(id).style.color=TIME_COLOR;
-		// switch (hour % 12) {
-		// 	case 0:
-		// 		console.log("twelve");
-		// 		document.getElementById("twelve").style.color="black";
-		// 		break;
-		// 	case 1:
-		// 		console.log("one");
-		// 		document.getElementById("one").style.color="#000000";
-		// 		break;
-		// 	case 2:
-		// 		console.log("two");
-		// 		break;
-		// 	case 3:
-		// 		console.log("three");
-		// 		break;
-		// 	case 4:
-		// 		console.log("four");
-		// 		break;
-		// 	case 5:
-		// 		console.log("five");
-		// 		break;
-		// 	case 6:
-		// 		console.log("six");
-		// 		break;
-		// 	case 7:
-		// 		console.log("seven");
-		// 		break;
-		// 	case 8:
-		// 		console.log("eight");
-		// 		break;
-		// 	case 9:
-		// 		console.log("nine");
-		// 		break;
-		// 	case 10:
-		// 		console.log("ten");
-		// 		break;
-		// 	case 11:
-		// 		console.log("eleven");
-		// 		break;
-		// 	default:
-		// 		console.log("getHour() - default - THIS IS WRONG!!!")
-		// }
 }
 
 function clearStyle() {
-	document.getElementById("one").removeAttribute("style");
+	var elements;
+
+	elements = document.querySelectorAll('.time');
+	for (var i = 0; i < elements.length; i++) {
+		elements[i].style.color = "#EEEEEE";
+	}
 }
 
 function testGetMinutes() {
 	for (var i = 0; i < 60; i++) {
-		// console.log(i + ": ");
 		getMinute(i);
 	}
 }
@@ -154,12 +105,40 @@ function testGetHours() {
 	}
 }
 
+function buttonPress(e) {
+	var e = e || window.event;
+	var btnCode;
+	var elements;
+
+	clearStyle();
+
+	if ('object' === typeof e) {
+		btnCode = e.button;
+
+		switch(btnCode) {
+			case 0:
+			case 1:
+			case 2:
+				elements = document.querySelectorAll('.name');
+				for (var i = 0; i < elements.length; i++) {
+					elements[i].style.color = IAN_COLOR;
+				}
+				// .style.color = IAN_COLOR;
+				break;
+			default:
+				console.log("Unexpected code: " + btnCode);
+		}
+	}
+}
+
 // var time = getCurrentTime();
 // console.log(time.hours);
 // console.log(time.minutes);
 
 // clearStyle();
 decodeTime();
-testGetMinutes();
-testGetHours();
+
+
+// testGetMinutes();
+// testGetHours();
 // testGetMinutes();
