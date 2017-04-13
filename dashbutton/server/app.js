8b6486bef3f5811d2c7d809805490d41d70c5e36
nfang <fang.nicholas@gmail.com>
2017-04-13 14:03:33 -0500

    Creating a server that will capture when an Amazon Dash button is pressed and hopefully update a count that is then displayed on a screen.

---
+++ dashbutton/server/app.js
@@ -0,0 +1,35 @@
+const express    = require("express");
+const dashButton = require("node-dash-button");
+const serverEvent = require("server-event");
+
+const app        = express();
+const port       = 3000;
+
+let count = 0;
+
+// dang button mac address: 68:54:fd:72:f4:74
+// iface
+// 10 second delay before looking for another button push
+// use both arp and udp protocol
+var dash = dashButton("68:54:fd:72:f4:74", null, 10000, 'all');
+
+serverEvent = serverEvent({ express: app });
+
+app.get("/", (req, res) => {
+   res.sendFile("dash.html");
+	dash.on("detected", () => {
+	   console.log("Button press detected, now figure out something for me to do!");
+	   count++;
+	   // send event
+	   serverEvent(req, res);
+	   res.sse('buttonCount', `Button press #: ${count}`);
+	});
+});
+
+app.listen(port, (err) => {
+   if (err) {
+      return console.log("Something bad happened", err);
+   }
+   console.log(`Server is listening on ${port}`);
+});
+
