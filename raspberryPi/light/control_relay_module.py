4df04fe98db24cba9ee31bb5d7d5a190a2fe100f
nickfang <fang.nicholas@gmail.com>
2026-09-03 09:42:19 -0500

    updating script to turn on light for 5 minutes and then turn it off.

--- raspberryPi/light/control_relay_module.py
+++ raspberryPi/light/control_relay_module.py
@@ -1,8 +1,8 @@
 import RPi.GPIO as GPIO
 import time
 
-PIN_RELAY_1 = 2
-PIN_RELAY_2 = 3
+PIN_RELAY_1 = 14
+PIN_RELAY_2 = 15
 
 GPIO.setmode(GPIO.BCM)
 
@@ -10,16 +10,14 @@ GPIO.setup(PIN_RELAY_1, GPIO.OUT)
 GPIO.setup(PIN_RELAY_2, GPIO.OUT)
 
 try:
-	while True:
-		print("Turn on all 2 relays")
-		GPIO.output(PIN_RELAY_1, GPIO.HIGH)
-		GPIO.output(PIN_RELAY_2, GPIO.HIGH)
-		time.sleep(1)
+    print("Turn on relay 1 & 2 ")
+	GPIO.output(PIN_RELAY_1, GPIO.HIGH)
+	GPIO.output(PIN_RELAY_2, GPIO.HIGH)
+	time.sleep(300)
 
-		print("Turn off all 2 relays")
-		GPIO.output(PIN_RELAY_1, GPIO.LOW)
-		GPIO.output(PIN_RELAY_2, GPIO.LOW)
-		time.sleep(1)
+	print("Turn off relays 1 & 2")
+	GPIO.output(PIN_RELAY_1, GPIO.LOW)
+	GPIO.output(PIN_RELAY_2, GPIO.LOW)
 
 except KeyboardInterrupt:
 	GPIO.cleanup()
