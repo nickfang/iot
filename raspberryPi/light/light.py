ba23f510e42c5e9ca6289ddecfa909a1f87f2c77
nickfang <fang.nicholas@gmail.com>
2026-09-06 08:51:23 -0500

    Updaing light.py to handle clean up correctly and added functions for controlling one at a time or all at a time.

--- raspberryPi/light/light.py
+++ raspberryPi/light/light.py
@@ -1,24 +1,34 @@
 import RPi.GPIO as GPIO
 import time
 
-DEFAULT_PIN_RELAY_1 = 14
-DEFAULT_PIN_RELAY_2 = 15
-DEFAULT_SECONDS = 300
+PIN_RELAY_1 = 14
+PIN_RELAY_2 = 15
+DELAY_SECONDS = 300
 ON, OFF = GPIO.LOW, GPIO.HIGH
 
 
 def setup():
     GPIO.setmode(GPIO.BCM)
-    GPIO.setup(DEFAULT_PIN_RELAY_1, GPIO.OUT, initial=OFF)
-    GPIO.setup(DEFAULT_PIN_RELAY_2, GPIO.OUT, initial=OFF)
+    GPIO.setup(PIN_RELAY_1, GPIO.OUT, initial=OFF)
+    GPIO.setup(PIN_RELAY_2, GPIO.OUT, initial=OFF)
 
-def on(pin=DEFAULT_PIN_RELAY_1):
+def invalid_pin(pin):
+    if pin != PIN_RELAY_1 and pin != PIN_RELAY_2:
+        print("Invalid pin: ", pin)
+        return True
+    return False
+
+def on(pin=PIN_RELAY_1):
+    if invalid_pin(pin):
+        return
     GPIO.output(pin, ON)
 
-def off(pin=DEFAULT_PIN_RELAY_1):
+def off(pin=PIN_RELAY_1):
+    if invalid_pin(pin):
+        return
     GPIO.output(pin, OFF)
 
-def toggle_with_delay(pin=DEFAULT_PIN_RELAY_1, start=ON, delay=DEFAULT_SECONDS):
+def toggle_with_delay(pin=PIN_RELAY_1, start=ON, delay=DELAY_SECONDS):
     print("Start toggle.", "ON" if start == ON else OFF)
     GPIO.output(pin, start)
     print("Start delay.", delay)
@@ -27,13 +37,29 @@ def toggle_with_delay(pin=DEFAULT_PIN_RELAY_1, start=ON, delay=DEFAULT_SECONDS):
     GPIO.output(pin, OFF if start == ON else ON)
     print("End toggle.", "OFF" if start == ON else ON)
         
-def cleanup(pin=DEFAULT_PIN_RELAY_1):
-    GPIO.output(pin, OFF)
-    GPIO.cleanup(pin)
+def all_on():
+    GPIO.output(PIN_RELAY_1, ON)
+    GPIO.output(PIN_RELAY_2, ON)
+
+def all_off():
+    GPIO.output(PIN_RELAY_1, OFF)
+    GPIO.output(PIN_RELAY_2, OFF)
+
+def all_toggle_with_delay(start=ON, delay=DELAY_SECONDS):
+    GPIO.output(PIN_RELAY_1, start)
+    GPIO.output(PIN_RELAY_2, start)
+    time.sleep(delay)
+    GPIO.output(PIN_RELAY_1, OFF if start == ON else ON)
+    GPIO.output(PIN_RELAY_2, OFF if start == ON else ON)
+
+def cleanup():
+    GPIO.output(PIN_RELAY_1, OFF)
+    GPIO.output(PIN_RELAY_2, OFF)
+    GPIO.cleanup(PIN_RELAY_1)
+    GPIO.cleanup(PIN_RELAY_2)
 
 if __name__ == "__main__":
-    pin = 14
-    delay = 10
-    toggle_with_delay(pin, ON, delay)
-    cleanup(pin)
+    setup()
+    all_toggle_with_delay(ON, 10)
+    cleanup()
 
